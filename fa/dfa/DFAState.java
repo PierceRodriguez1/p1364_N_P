fb0e2fafc068f663f237496d3f971c15265fc536
Nolan Stetz <nolanstetz@u.boisestate.edu>
2024-02-13 15:04:41 -0700

    DFAState.java update

--- fa/dfa/DFAState.java
+++ fa/dfa/DFAState.java
@@ -1,14 +1,24 @@
 package fa.dfa;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import fa.State;
 
 public class DFAState extends State{
-
+    private Map<Character, State> transitionMap;
+    
     public DFAState(String name) {
         super(name);
+        transitionMap = new HashMap<>();
     }
 
-    
+    public void addTransition(char symbol, State nextState){
+        transitionMap.put(symbol, nextState);
+    }
 
-    
+    public State nextState(char symbol){
+        return transitionMap.get(symbol);
+    }
+
 }
