5ca7bec9416cbb84c16904bf9a0d495ec0cbca9c
Pierce <Piercerodriguez@u.boisestate.edu>
2024-02-15 22:21:00 -0700

    updates

--- fa/dfa/DFA.java
+++ fa/dfa/DFA.java
@@ -131,11 +131,11 @@ public class DFA implements DFAInterface{
         DFAState getState = new DFAState(name); //creating a new state to check against hashset instead of string
 
         if(states.contains(getState) == true){
-            return new DFAState(name);
+            return getState;
         } else if(finalStates.contains(getState) == true){
-            return new DFAState(name);
+            return getState;
         } else if (start.getName() == name){
-            return new DFAState(name);
+            return getState;
         }
 
         return null;
